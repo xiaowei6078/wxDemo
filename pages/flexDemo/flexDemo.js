52baace88724f1634f53833c1af5746e1bffc67b
xiaowei6078 <1218706950@qq.com>
2020-07-09 23:57:32 +0800

    优化

--- pages/flexDemo/flexDemo.js
+++ pages/flexDemo/flexDemo.js
@@ -1,38 +1,66 @@
 Page({
     data: {
-        containerStyle: {},
+        containerStyle: "",
         // 规定灵活项目的方向
-        flex: [
-            {
+        flex: {
+            flexDirection: {
                 title: "flex-direction(规定灵活项目的方向)",
                 value: "flex-direction",
                 children: [
                     {
                         name: "竖向排列(column)",
                         value: "column",
-                        checked: true
+                        checked: false
                     },
                     {
                         name: "竖向排列（元素以相反的顺序 - column-reverse)",
-                        value: "column-reverse"
+                        value: "column-reverse",
+                        checked: true
                     },
                     {
                         name: "横向排列(row)",
-                        value: "row"
+                        value: "row",
+                        checked: false
                     },
                     {
                         name: "横向排列（元素以相反的顺序 - row-reverse）",
-                        value: "row-reverse"
+                        value: "row-reverse",
+                        checked: false
                     },
                     {
                         name: "默认值",
-                        value: "initial"
+                        value: "initial",
+                        checked: false
                     }
                 ]
             }
-        ]
+        }
     },
     onLoad() {
-
+        this.formatStyle();
+    },
+    formatStyle() {
+        const attrData = this.data.flex;
+        const attrs = [];
+        for (let key in attrData) {
+            let children = attrData[key].children;
+            const chilItem = children.filter(item => item.checked);
+            if (chilItem[0]) {
+                attrs.push(`${attrData[key].value}: ${chilItem[0].value}`)
+            }
+        }
+        this.setData({
+            containerStyle: attrs.join(";")
+        });
+    },
+    attrChange(e) {
+        const key = e.target.dataset.key;
+        const val = e.detail.value;
+        const children = this.data.flex[key].children;
+        children.map(item => {
+            const isTrue = item.value === val;
+            item.checked = isTrue;
+        });
+        this.formatStyle();
     }
 })
